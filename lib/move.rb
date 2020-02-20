38e060246274d0d34d310938e89e7350e81ade71
Anastasia Orlova <metkusblackus@gmail.com>
2020-02-20 00:12:27 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,6 +10,6 @@ def input_to_index(user_input)
   user_input = user_input.to_i - 1
 end
 
-def move(board, index, current_player = "x", "0")
+def move(board, index, current_player = "X", 0)
   board[index] = current_player
 end
