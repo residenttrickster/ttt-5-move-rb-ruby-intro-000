98a4ce078ab348e711f5f0627c3d8c7d4ed3ec67
Anastasia Orlova <metkusblackus@gmail.com>
2020-02-20 00:08:52 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input = "1" - "9")
+def input_to_index(user_input)
   user_input = user_input.to_i - 1
 end
 
